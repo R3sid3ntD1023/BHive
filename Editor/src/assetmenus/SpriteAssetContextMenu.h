fd8b0e4d0eb848c473509c69c8955ca6a000ad7e
R3sid3ntD1023 <dariusbigbee12@gmail.com>
2025-01-17 23:33:05 -0500

    moved editors to editors folder

--- Editor/src/assetmenus/SpriteAssetContextMenu.h
+++ Editor/src/assetmenus/SpriteAssetContextMenu.h
@@ -1,80 +1,79 @@
 #pragma once
 
 #include "asset/FAssetContextMenu.h"
-#include "windows/SpriteEditor.h"
-#include "windows/FlipBookEditor.h"
-#include "windows/AudioEditor.h"
-#include "windows/PhysicsMaterialEditor.h"
-#include "windows/InputContextEditor.h"
-#include "windows/MaterialEditor.h"
-#include "windows/SkeletonEditor.h"
-#include "windows/MeshEditor.h"
-#include "windows/AnimationEditor.h"
-#include "windows/TextureEditor.h"
+#include "windows/editors/AnimationEditor.h"
+#include "windows/editors/AudioEditor.h"
+#include "windows/editors/FlipBookEditor.h"
+#include "windows/editors/InputContextEditor.h"
+#include "windows/editors/MaterialEditor.h"
+#include "windows/editors/MeshEditor.h"
+#include "windows/editors/PhysicsMaterialEditor.h"
+#include "windows/editors/SkeletonEditor.h"
+#include "windows/editors/SpriteEditor.h"
+#include "windows/editors/TextureEditor.h"
 
 namespace BHive
 {
-    class AssetEditor;
+	class AssetEditor;
 
-    template <typename TEditor>
-    struct FAssetContextMenuBase : public FAssetContextMenu
-    {
-        virtual void OnAssetContext(const AssetHandle &handle) override;
-        virtual void OnAssetOpen(const AssetHandle &handle) override;
+	template <typename TEditor>
+	struct FAssetContextMenuBase : public FAssetContextMenu
+	{
+		virtual void OnAssetContext(const AssetHandle &handle) override;
+		virtual void OnAssetOpen(const AssetHandle &handle) override;
+	};
 
-    };
+	struct SpriteAssetContextMenu : public FAssetContextMenuBase<SpriteEditor>
+	{
+		REFLECTABLEV(FAssetContextMenu)
+	};
 
-    struct SpriteAssetContextMenu : public FAssetContextMenuBase<SpriteEditor>
-    {
-        REFLECTABLEV(FAssetContextMenu)
-    };
+	struct FlipbookAssetContextMenu : public FAssetContextMenuBase<FlipBookEditor>
+	{
+		REFLECTABLEV(FAssetContextMenu)
+	};
 
-    struct FlipbookAssetContextMenu : public FAssetContextMenuBase<FlipBookEditor>
-    {
-        REFLECTABLEV(FAssetContextMenu)
-    };
+	struct AudioAssetContextMenu : public FAssetContextMenuBase<AudioEditor>
+	{
+		REFLECTABLEV(FAssetContextMenu)
+	};
 
-    struct AudioAssetContextMenu : public FAssetContextMenuBase<AudioEditor>
-    {
-        REFLECTABLEV(FAssetContextMenu)
-    };
+	struct PhysicsMaterialContextMenu : public FAssetContextMenuBase<PhysicsMaterialEditor>
+	{
+		REFLECTABLEV(FAssetContextMenu)
+	};
 
-    struct PhysicsMaterialContextMenu : public FAssetContextMenuBase<PhysicsMaterialEditor>
-    {
-        REFLECTABLEV(FAssetContextMenu)
-    };
+	struct SpriteSheetContextMenu : public FAssetContextMenuBase<SpriteSheetEditor>
+	{
+		REFLECTABLEV(FAssetContextMenu)
+	};
 
-    struct SpriteSheetContextMenu : public FAssetContextMenuBase<SpriteSheetEditor>
-    {
-        REFLECTABLEV(FAssetContextMenu)
-    };
+	struct InputContextContextMenu : public FAssetContextMenuBase<InputContextEditor>
+	{
+		REFLECTABLEV(FAssetContextMenu)
+	};
 
-    struct InputContextContextMenu : public FAssetContextMenuBase<InputContextEditor>
-    {
-        REFLECTABLEV(FAssetContextMenu)
-    };
+	struct MaterialContextMenu : public FAssetContextMenuBase<MaterialEditor>
+	{
+		REFLECTABLEV(FAssetContextMenu)
+	};
 
-    struct MaterialContextMenu : public FAssetContextMenuBase<MaterialEditor>
-    {
-        REFLECTABLEV(FAssetContextMenu)
-    };
+	struct SkeletonContextMenu : public FAssetContextMenuBase<SkeletonEditor>
+	{
+		REFLECTABLEV(FAssetContextMenu)
+	};
 
-    struct SkeletonContextMenu : public FAssetContextMenuBase<SkeletonEditor>
-    {
-        REFLECTABLEV(FAssetContextMenu)
-    };
+	struct MeshContextMenu : public FAssetContextMenuBase<MeshEditor>
+	{
+		REFLECTABLEV(FAssetContextMenu)
+	};
 
-    struct MeshContextMenu : public FAssetContextMenuBase<MeshEditor>
-    {
-        REFLECTABLEV(FAssetContextMenu)
-    };
+	struct AnimationContextMenu : public FAssetContextMenuBase<AnimationEditor>
+	{
+		REFLECTABLEV(FAssetContextMenu)
+	};
 
-    struct AnimationContextMenu : public FAssetContextMenuBase<AnimationEditor>
-    {
-        REFLECTABLEV(FAssetContextMenu)
-    };
-
-    struct TextureContextMenu : public FAssetContextMenuBase<TextureEditor>
+	struct TextureContextMenu : public FAssetContextMenuBase<TextureEditor>
 	{
 		REFLECTABLEV(FAssetContextMenu)
 	};
