2b3a56b450dedbd1679257aaf12553aac587c74e
dbee00 <dariusbigbee12@gmail.com>
2025-02-08 15:54:40 -0500

    updated serialize functions for archive types

--- Engine/src/BHive/core/serialization/types/glm.hpp
+++ Engine/src/BHive/core/serialization/types/glm.hpp
@@ -1,31 +1,40 @@
 #pragma once
 
 #include "math/Math.h"
+#include <cereal/archives/binary.hpp>
 #include <cereal/cereal.hpp>
 #include <string>
 
 namespace glm
 {
-	/*template <typename A, length_t L, typename T, qualifier Q>
-	inline void CEREAL_SERIALIZE_FUNCTION_NAME(A &ar, vec<L, T, Q> &obj)
-	{
-		ar(cereal::binary_data(&obj, sizeof(vec<L, T, Q>)));
-	}*/
+	template <typename A>
+	constexpr bool is_json_archive_v = std::is_same_v<A, cereal::JSONOutputArchive> || std::is_same_v<A, cereal::JSONInputArchive>;
 
-	template <typename TArchive, length_t C, length_t R, typename T, qualifier Q>
-	inline void CEREAL_SERIALIZE_FUNCTION_NAME(TArchive &ar, mat<C, R, T, Q> &obj)
+	template <typename A>
+	constexpr bool is_binary_archive_v = std::is_same_v<A, cereal::BinaryOutputArchive> || std::is_same_v<A, cereal::BinaryInputArchive>;
+
+	template <typename A, length_t C, length_t R, typename T, qualifier Q, std::enable_if_t<is_json_archive_v<A>, bool> = true>
+	inline void CEREAL_SERIALIZE_FUNCTION_NAME(A &ar, mat<C, R, T, Q> &obj)
 	{
-		ar(cereal::binary_data(&obj, sizeof(mat<C, R, T, Q>)));
+		ar(cereal::make_size_tag(C * R));
+		for (int i = 0; i < C; i++)
+		{
+			for (int j = 0; j < R; j++)
+			{
+				ar(obj[i][j]);
+			}
+		}
 	}
 
-	template <typename A, typename T, qualifier Q>
+	template <typename A, typename T, qualifier Q, std::enable_if_t<is_json_archive_v<A>, bool> = true>
 	inline void CEREAL_SERIALIZE_FUNCTION_NAME(A &ar, qua<T, Q> &obj)
 	{
-		ar(cereal::binary_data(&obj, sizeof(qua<T, Q>)));
+		ar(cereal::make_size_tag(4));
+		ar(obj.x, obj.y, obj.z, obj.w);
 	}
 
-	template <typename A, length_t L, typename T, qualifier Q>
-	inline void CEREAL_SAVE_FUNCTION_NAME(A &ar, const vec<L, T, Q> &obj)
+	template <typename A, length_t L, typename T, qualifier Q, std::enable_if_t<is_json_archive_v<A>, bool> = true>
+	inline void CEREAL_SERIALIZE_FUNCTION_NAME(A &ar, vec<L, T, Q> &obj)
 	{
 		ar(cereal::make_size_tag(L));
 		for (int i = 0; i < L; i++)
@@ -34,15 +43,22 @@ namespace glm
 		}
 	}
 
-	template <typename A, length_t L, typename T, qualifier Q>
-	inline void CEREAL_LOAD_FUNCTION_NAME(A &ar, vec<L, T, Q> &obj)
+	template <typename A, length_t C, length_t R, typename T, qualifier Q, std::enable_if_t<is_binary_archive_v<A>, bool> = true>
+	inline void CEREAL_SERIALIZE_FUNCTION_NAME(A &ar, mat<C, R, T, Q> &obj)
 	{
-		size_t size = L;
-		ar(cereal::make_size_tag(size));
-		for (int i = 0; i < L; i++)
-		{
-			ar(obj[i]);
-		}
+		ar(cereal::binary_data(&obj, sizeof(obj)));
+	}
+
+	template <typename A, typename T, qualifier Q, std::enable_if_t<is_binary_archive_v<A>, bool> = true>
+	inline void CEREAL_SERIALIZE_FUNCTION_NAME(A &ar, qua<T, Q> &obj)
+	{
+		ar(cereal::binary_data(&obj, sizeof(obj)));
+	}
+
+	template <typename A, length_t L, typename T, qualifier Q, std::enable_if_t<is_binary_archive_v<A>, bool> = true>
+	inline void CEREAL_SERIALIZE_FUNCTION_NAME(A &ar, vec<L, T, Q> &obj)
+	{
+		ar(cereal::binary_data(&obj, sizeof(obj)));
 	}
 
 } // namespace glm
