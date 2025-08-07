302fd0b4d7d2799e390f4e249b0e9ee9cc37f885
dbee00 <dariusbigbee12@gmail.com>
2025-08-07 19:31:19 -0400

    changed profiler output location

--- Engine/src/BHive/core/EntryPoint.h
+++ Engine/src/BHive/core/EntryPoint.h
@@ -16,16 +16,16 @@ namespace BHive
 	{
 		BHive::Log::Init();
 		BHive::AudioContext::Init();
-		BH_PROFILE_BEGIN_SESSION("StartUp", ENGINE_PATH "/../Profile-StartUp.json");
+		BH_PROFILE_BEGIN_SESSION("StartUp", "Profile-StartUp.json");
 		auto app = BHive::CreateApplication({argc, argv});
 
 		BH_PROFILE_END_SESSION();
 
-		BH_PROFILE_BEGIN_SESSION("Runtime", ENGINE_PATH "/../Profile-Runtime.json");
+		BH_PROFILE_BEGIN_SESSION("Runtime", "Profile-Runtime.json");
 		app->Run();
 		BH_PROFILE_END_SESSION();
 
-		BH_PROFILE_BEGIN_SESSION("Shutdown", ENGINE_PATH "/../Profile-Shutdown.json");
+		BH_PROFILE_BEGIN_SESSION("Shutdown", "Profile-Shutdown.json");
 		delete app;
 		BH_PROFILE_END_SESSION();
 
