6324097fc88544b04aa5f2cd02a7daadcc25c390
dbee00 <dariusbigbee12@gmail.com>
2025-08-08 01:13:47 -0400

    refleced push constants

--- Engine/src/BHive/gfx/ShaderReflection.cpp
+++ Engine/src/BHive/gfx/ShaderReflection.cpp
@@ -31,6 +31,29 @@ namespace BHive
 			UniformBuffers[uniform.name] = buffer_data;
 		}
 
+		for (const auto &uniform : resources.push_constant_buffers)
+		{
+			auto &buffer = compiler.get_type(uniform.base_type_id);
+
+			FUniformBufferData buffer_data;
+			buffer_data.Binding = compiler.get_decoration(uniform.id, spv::DecorationBinding);
+			buffer_data.Size = compiler.get_declared_struct_size(buffer);
+			auto member_count = buffer.member_types.size();
+			for (size_t i = 0; i < member_count; i++)
+			{
+				auto &member = buffer.member_types[i];
+				auto &type = compiler.get_type(member);
+
+				FUniform uniform_data;
+				uniform_data.Type = type.basetype;
+				uniform_data.Size = type.width / 8;
+				uniform_data.Offset = compiler.type_struct_member_offset(buffer, i);
+				uniform_data.Location = compiler.get_decoration(member, spv::DecorationLocation);
+				buffer_data.Uniforms[compiler.get_member_name(buffer.self, i)] = uniform_data;
+			}
+			UniformBuffers[uniform.name] = buffer_data;
+		}
+
 		for (auto &uniform : resources.gl_plain_uniforms)
 		{
 			auto &type = compiler.get_type(uniform.base_type_id);
@@ -72,16 +95,12 @@ namespace BHive
 			result += fmt::format("\t\tUniform Buffer: {} - Binding: {} - Size: {}\n", name, buffer.Binding, buffer.Size);
 			for (const auto &[uniform_name, uniform] : buffer.Uniforms)
 			{
-				result += fmt::format(
-					"\t\tUniform: {} - Type: {} - Size: {} - Offset: {} - Location: {}\n", uniform_name, uniform.Type,
-					uniform.Size, uniform.Offset, uniform.Location);
+				result += fmt::format("\t\tUniform: {} - Type: {} - Size: {} - Offset: {} - Location: {}\n", uniform_name, uniform.Type, uniform.Size, uniform.Offset, uniform.Location);
 			}
 		}
 		for (const auto &[name, uniform] : Uniforms)
 		{
-			result += fmt::format(
-				"\t\tUniform: {} - Type: {} - Size: {} - Offset: {} - Location: {}\n", name, uniform.Type, uniform.Size,
-				uniform.Offset, uniform.Location);
+			result += fmt::format("\t\tUniform: {} - Type: {} - Size: {} - Offset: {} - Location: {}\n", name, uniform.Type, uniform.Size, uniform.Offset, uniform.Location);
 		}
 		for (const auto &[name, buffer] : StorageBuffers)
 		{
