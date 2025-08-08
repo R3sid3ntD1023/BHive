a1d6e7df3df748ac0aa647bd9da16509d2ae9203
dbee00 <dariusbigbee12@gmail.com>
2025-08-08 01:12:18 -0400

    updated planet shader

--- projects/SolarSystem/Resources/Shaders/Planet.glsl
+++ projects/SolarSystem/Resources/Shaders/Planet.glsl
@@ -1,119 +1,46 @@
 #type vertex
 
 #version 460 core
+
 #include <Core.glsl>
-
-layout(location = 0) in vec3 vPos;
-layout(location = 1) in vec2 vTexCoord;
-layout(location = 2) in vec3 vNormal;
-
-struct PerObjectData
-{
-	mat4 WorldMatrix;
-};
-
-layout(std430, binding = 0) uniform CameraBuffer
-{
-	mat4 u_projection;
-	mat4 u_view;
-	vec2 u_near_far;
-	vec3 u_camera_position;
-};
-
-layout(std430, binding = 1) restrict readonly buffer PerObjectSSBO
-{
-	PerObjectData object[];
-};
-
-layout(std430, binding = 2) restrict readonly buffer InstanceSSBO
-{
-	mat4 instances[];
-};
-
-
-layout(location = 0) out struct VS_OUT
-{
-	vec3 position;
-	vec2 texcoord;
-	vec3 normal;
-} vs_out;
+#include <CommonParamsVert.glsl>
 
 void main()
 {
-
-	bool instanced = gl_InstanceIndex != -1; 
-
-	mat4 instance = mix( instances[gl_InstanceIndex],  mat4(1), float(instanced));
-
-	mat4 model = object[gl_DrawID].WorldMatrix * instance;
-	vec4 worldPos = model * vec4(vPos, 1);
-	gl_Position = u_projection * u_view * worldPos;
-
-	vs_out.position = worldPos.xyz;
-	vs_out.texcoord = vTexCoord;
-	mat3 normal_matrix = transpose(inverse(mat3(model)));
-	vs_out.normal = normal_matrix * vNormal;
+	#include <CommonVert.glsl>
 }
 
 #type fragment
 
 #version 460 core
 
-#define SINGLE_CHANNEL 1 << 0
+#include <Core.glsl>
+#include <CommonParamsFrag.glsl>
+#include <Lighting.glsl>
+#include <LambertMaterial.glsl>
+#include <LightingParamsFrag.glsl>
+#include <DiffuseMapFrag.glsl>
 
-layout(location = 0) in struct VS_OUT
-{
-	vec3 position;
-	vec2 texcoord;
-	vec3 normal;
-} vs_in;
-
-layout(push_constant) uniform Material
-{
-	vec3 uColor;
-	vec3 uEmission;
-	uint uFlags;
-} material;
-
-layout(binding = 0) uniform sampler2D uTexture;
-
-layout(std430, binding = 0) uniform CameraBuffer
-{
-	mat4 u_projection;
-	mat4 u_view;
-	vec2 u_near_far;
-	vec3 u_camera_position;
-};
 
 layout(location = 0) out vec4 fColor;
-layout(location = 1) out vec4 fPosition;
-layout(location = 2) out vec4 fNormal;
-layout(location = 3) out vec4 fEmission;
-
-
-float LinerizeDepth(float depth, float near, float far);
-
 
 void main()
 {
-	uint is_single_channel = (material.uFlags & SINGLE_CHANNEL);
+	vec4 diffuseColor = constants.u_Material.DiffuseColor;
+	vec3 totalEmissiveRadiance = constants.u_Material.EmissionColor;
 
-	vec3 color = pow(texture(uTexture, vs_in.texcoord).rgb, vec3(2.2));
-	color.rgb = mix(color.rgb, vec3(color.r), is_single_channel);
+	#include <UseDiffuseMapFrag.glsl>
 
-	color += vec3(.1) * material.uColor;
-	fColor = vec4(color, 1);
-	fPosition = vec4(vs_in.position, 1);
-	fNormal = vec4(normalize(vs_in.normal), 1);
-	fEmission = vec4(material.uEmission, 1);
+	LambertMaterial material;
+	material.DiffuseColor = diffuseColor;
+	material.EmissionColor = totalEmissiveRadiance;
 
-	//gl_FragDepth = LinerizeDepth(gl_FragCoord.z, u_near_far.x, u_near_far.y);
-}
+	ReflectedLight reflected_light = ReflectedLight(vec3(0.0), vec3(0.0), vec3(0.0), vec3(0.0));
+	
+	#include <CalculateNormals.glsl>
+	#include <LightingFrag.glsl>
+	
+	vec3 out_color = reflected_light.DirectDiffuse + totalEmissiveRadiance;
 
-float LinerizeDepth(float depth, float near, float far)
-{
-    float ndc = 2 * depth - 1;
-    float eye = (2 * near * far) / ((far + near) - ndc * (far -near));
-    float linearDepth = (eye - near) / (far - near);
-    return linearDepth;
+	#include <OutgoingFragment.glsl>
 }
